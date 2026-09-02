0e7d3f48bf4641af2a7377a51a6fa9a4eb889d9e
Md. Azizul Hakim <aazizul924@gmail.com>
2026-09-03 00:22:50 +0600

    Question 2

--- assignment_1/answer.js
+++ assignment_1/answer.js
@@ -1,14 +1,35 @@
+// Question 1
 function describeValue(value) {
     let type = typeof value;
     let truthiness;
-    
+
     if (value) {
         truthiness = "truthy";
     } else {
         truthiness = "falsy";
     }
-    
-return type + " | " + truthiness;
+
+    return type + " | " + truthiness;
 }
 
 console.log(describeValue(null))
+
+
+// Question 2
+function getDayType(day) {
+    let lowerCaseDay = day.toLowerCase();
+
+    switch (lowerCaseDay) {
+        case "friday":
+        case "saturday":
+            return "Weekend";
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+        default:
+            return "Invalid Day";
+    }
+}
