51e28fa35ff9a3f53f8ce743df3dbe1bfaae49ae
Md. Azizul Hakim <aazizul924@gmail.com>
2026-09-03 01:12:36 +0600

    question 4

--- assignment_1/answer.js
+++ assignment_1/answer.js
@@ -50,3 +50,22 @@ function validateUsername(name) {
         return "Available"
     }
 }
+
+
+// Question 4
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+
+    let fare = 50;
+
+    if (distance > 2) {
+        fare = fare + (distance - 2) * 15;
+    }
+
+    fare = fare + waitingMinutes * 2;
+
+    if (isNight) {
+        fare = fare * 1.20;
+    }
+
+    return fare;
+}
